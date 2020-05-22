e8bc5eceb7df8b5f16c34a352ceb9e3c53b9aa74
AravGarg <arav1635@gmail.com>
2020-05-22 23:53:48 +0530

    added fastbin_dup demo

---
+++ techniques/fastbin_dup.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+int main()
+{
+	char *a=malloc(0x8);
+	char *b=malloc(0x8);
+	char *c=malloc(0x8);
+	
+	printf("first chunk @ %p.\n",a);
+	printf("Second chunk @ %p.\n",b);
+	printf("Third chunk @ %p.\n",c);
+
+	free(a);
+	free(b);
+	free(a);
+
+	printf("After double free\n");
+	
+	char *d=malloc(0x8);
+	char *e=malloc(0x8);
+	char *f=malloc(0x8);
+
+
+	printf("first chunk @ %p.\n",d);
+	printf("Second chunk @ %p.\n",e);
+	printf("Fourth chunk @ %p.\n",f);
+}
