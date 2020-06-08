ba9522840a356baa4e7d010d0d3baccb741b7857
arav <arav@Ubuntu16.04>
2020-06-08 15:24:09 +0530

    Added demo for fastbin dup into stack

--- techniques/fastbin_dup_into_stack/fastbin_dup_into_stack.c
+++ techniques/fastbin_dup_into_stack/fastbin_dup_into_stack.c
@@ -5,19 +5,23 @@
 
 int main()
 {	
-	int d;
-	while(1){d=getchar();
-	getchar();
-	if(d==49){
+	unsigned long long d[3];
 	unsigned long long *p0,*p1,*p2,*p3;
-	
+	p0=malloc(0x8);
+	p1=malloc(0x8);
+	p2=malloc(0x8);
 
+	free(p0);
+	free(p1);
+	free(p0);
 
+	d[0]=0x10000;
+	d[1]=0x20;
+	d[2]=0x0;
+	*p0=d;
 
+	malloc(0x8);
+	p3=malloc(0x8);
 
-
-
-	}
-	else{break;}}
-	
+	printf("p3 @ %p\n",p3);
 }
